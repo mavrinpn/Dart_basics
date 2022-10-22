c9eca854d0577d674d4fdeee2eb73b817b2dc532
Pavel Mavrin <mavrinpn@gmail.com>
2022-10-22 17:29:13 +0400

    Task 2

--- main.dart
+++ main.dart
@@ -1,15 +1,17 @@
 void main() {
 
   // Task 1
-  print("gcd = " + DelimetersCalculator().gcd(9, 6).toString());
-  print("lcm = " + DelimetersCalculator().lcm(16, 6).toString());
+  print("Calculated GCD = " + DelimetersCalculator().gcd(9, 6).toString());
+  print("Calculated LCM = " + DelimetersCalculator().lcm(16, 6).toString());
 
-  
+  // Task 2
+  print("Decimal int to Binary String = " + 145.toBIN());
+  print("Pseudo Binary int to Decimal int = " + 10010001.toDEC().toString());
+  print("Binary String to Decimal int = " + "10010001".toDEC().toString());
 }
 
 
 class DelimetersCalculator {
-
   int gcd(int a, int b) {
     if (a < b) {
         // Swap
@@ -34,3 +36,23 @@ class DelimetersCalculator {
   }
 }
 
+extension on int {
+  String toBIN() {
+    return this.toRadixString(2);
+  }
+
+  int? toDEC() {
+    String _string = this.toString();
+    return _string.toDEC();
+  }
+}
+
+extension on String {
+  int? toDEC() {
+    if (RegExp(r'^[0-1]+$').hasMatch(this)) { // require 0 and 1 only
+      return int.parse(this, radix: 2);
+    } else {
+      return null;
+    }
+  }
+}
