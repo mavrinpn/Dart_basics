0f34d0be2aa4e1a3dc968594a347bdb7450051db
Pavel Mavrin <mavrinpn@gmail.com>
2022-10-22 16:58:28 +0400

    Task 1

--- main.dart
+++ main.dart
@@ -1,3 +1,36 @@
 void main() {
-  print('tst');
+
+  // Task 1
+  print("gcd = " + DelimetersCalculator().gcd(9, 6).toString());
+  print("lcm = " + DelimetersCalculator().lcm(16, 6).toString());
+
+  
 }
+
+
+class DelimetersCalculator {
+
+  int gcd(int a, int b) {
+    if (a < b) {
+        // Swap
+        var tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    while (b != 0) {
+        a %= b;
+        // Swap
+        var tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    return a;
+  }
+
+  int lcm(int a, int b) {
+    return a ~/ gcd(a, b) * b;
+  }
+}
+
